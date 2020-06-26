fda5c6bb7d6ca934a8a80be6131bfeffeca247ef
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-06-25 21:50:26 -0400

    added styling

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,14 +1,49 @@
 import React from "react";
+import styled, {css} from "styled-components"
+import "../index.css";
+
+export const H1 = styled.h1`
+font-family: 'Notable', sans-serif ;
+ color: navy ;
+ filter: brightness(30%);
+
+ &:hover 
+`;
+
+export const Img = styled.img`
+    opacity: 1;  
+  &:hover {
+      opacity: 0.8;
+    
+  }    
+`;
+
+export const H2 = styled.h2`
+font-family: 'Notable', sans-serif ;
+color: navy ;
+filter: brightness(40%); 
+
+`;
+
+export const P = styled.p`
+  font-family: 'Bebas Neue', cursive;
+  color: navy;
+  filter:brightness(50%);
+
+`;
+
+
+
 
 const InfoCard = ({image, title, date, explanation }) => {
 
     return (
         <div className = 'dataPresentation'>
-            <h1>{title}</h1>
-            <img src = {image} alt = "bright clouds and a tiny upside down silouette of a big city" />
-            <h2>Photo Title: {title}</h2>
-            <p>Date Taken: {date}</p>
-            <p>About: {explanation}</p>
+            <H1>{title}</H1>
+            <Img src = {image} alt = "bright clouds and a tiny upside down silouette of a big city" />
+            <H2>Photo Title: {title}</H2>
+            <P>Date Taken: {date}</P>
+            <P>About: {explanation}</P>
         </div>
     );
 };
